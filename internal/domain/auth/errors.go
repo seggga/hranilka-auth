f44c21173437be2c95f6c11f09fe5d1622cbd7d6
Serg Kotovsky <rokulab@mail.ru>
2023-02-09 23:16:47 +0300

    add auth errors

--- internal/domain/auth/errors.go
+++ internal/domain/auth/errors.go
@@ -6,4 +6,6 @@ var (
 	ErrPassIncorrect = errors.New("password mismatch")
 	ErrEmptySecret   = errors.New("empty secret key for token creation")
 	ErrZeroDuration  = errors.New("zero duration for token creation")
+	ErrEmptyLogin    = errors.New("cannot create a user with empty login")
+	ErrPassTooLong   = errors.New("given password is too long")
 )
