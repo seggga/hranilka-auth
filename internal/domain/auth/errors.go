acd8f6118fe4e7f441891dc8e5d13dbfcecc50e3
Serg Kotovsky <rokulab@mail.ru>
2023-02-13 00:11:01 +0300

    add empty password error to domain.auth

--- internal/domain/auth/errors.go
+++ internal/domain/auth/errors.go
@@ -8,4 +8,5 @@ var (
 	ErrZeroDuration  = errors.New("zero duration for token creation")
 	ErrEmptyLogin    = errors.New("cannot create a user with empty login")
 	ErrPassTooLong   = errors.New("given password is too long")
+	ErrNoPass        = errors.New("no password provided")
 )
